84e6c87b51f6e8b39ca8dae32e46d59f40af78b5
metastable-void <git@yuka.h09.eu>
2022-06-06 22:46:12 +0900

    Initial Commit

---
+++ MenheraInput/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MenheraInput
+//
+//  Created by Local Administrator on 2022/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenheraInput/MenheraInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenheraInputApp.swift
+//  MenheraInput
+//
+//  Created by Local Administrator on 2022/06/06.
+//
+
+import SwiftUI
+
+@main
+struct MenheraInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
